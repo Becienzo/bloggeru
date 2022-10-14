464ff8308d6fc5625d7d6cd9d1230814ac152c60
Becienzo <vincenzo.iovine94@gmail.com>
2022-10-14 22:41:39 +0100

    maven setup

---
+++ src/main/java/com/becienzo/bloggeru/BloggeruApplication.java
@@ -0,0 +1,13 @@
+package com.becienzo.bloggeru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggeruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/becienzo/bloggeru/BloggeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becienzo.bloggeru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
